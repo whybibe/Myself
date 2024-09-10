c27296e13b6e311fdbe2cc1ea93cbdae86611e05
whybibe <whybibe@gmail.com>
2024-09-10 13:00:34 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
-const width = document.getElementById("canvas").width = screen.availWidth;
-const height = document.getElementById("canvas").height = screen.availHeight;
-const ctx = document.getElementById("canvas").getContext("2d");
-const arr = Array(Math.ceil(width / 10)).fill(0);
-const str = "010101010101010101010101010101" + "1010101010101010101010101010".split("");
-ctx.font = "10px 优设标题黑";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// 设置画布的宽度和高度为视口的宽度和高度
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const arr = Array(Math.ceil(canvas.width / 10)).fill(0);
+const str = "01010101010101010101010101010110101010101010101010101010101010".split("");
+
+// 根据画布宽度动态设置字体大小
+const fontSize = canvas.width / 100; // 假设我们想要字体大小是画布宽度的1/100
+ctx.font = `${fontSize}px 优设标题黑`;
+
 function rain() {
-    ctx.fillStyle = "rgba(0,0,20,0.05)";
-    ctx.fillRect(0,0,width,height);
-    ctx.fillStyle = '#00c8aa';
+  ctx.fillStyle = "rgba(0,0,20,0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#00c8aa';
+
     arr.forEach(function (value, index){
         ctx.fillText(str[Math.floor(Math.random()*str.length)], index * 10, value + 10);
-        arr[index] = value >= height || value > 8888 * Math.random() ? 0 : value + 10;
+        arr[index] = value >= canvas.height || value > 8888 * Math.random() ? 0 : value + 10;
     });
 }
+
 setInterval(rain, 30);
