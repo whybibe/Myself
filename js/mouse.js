95c67fa8397aa369e679e134fdab025d4071895d
whybibe <whybibe@gmail.com>
2024-09-10 17:50:52 +0800

    Add files via upload

--- js/mouse.js
+++ js/mouse.js
@@ -70,3 +70,36 @@
         return "rgb("+(~~(Math.random()*255))+","+(~~(Math.random()*255))+","+(~~(Math.random()*255))+")";
     }
 })(window,document);
+
+(function() {
+    var coreSocialistValues = ["✿", "ˋˏ❤︎ˎˊ", "◍˃ᵕ˂◍", "•᷄ᯅ•᷅", "ʚ♡ɞ", "ฅ՞•ﻌ•՞ฅ", "༼ つ◕_◕ ༽つ", "♩♪♫♬♭♮♯", "დღ♡❣❤❥❦❧♥", "⍤⃝⍨⃝⍥⃝⍩⃝⃜❛˓", "☾˚‧º·ʜᴀ͟ᴘ͟ᴘ͟ʏᴇᴠᴇʀʏᴅᴀʏ̆̈❤️", "˃̣̣̥᷄⌓˂̣̣̥᷅"]
+        , index = Math.floor(Math.random() * coreSocialistValues.length);
+    document.body.addEventListener('click', function(e) {
+        if (e.target.tagName == 'A') {
+            return;
+        }
+        var x = e.pageX
+            , y = e.pageY
+            , span = document.createElement('span');
+        span.textContent = coreSocialistValues[index];
+        index = (index + 1) % coreSocialistValues.length;//取模循环
+        span.style.cssText = ['z-index: 9999999; position: absolute; font-weight: bold; color: #FFC0CB; top: ', y - 20, 'px; left: ', x, 'px;'].join('');
+        document.body.appendChild(span);
+        animate(span);
+    });
+    function animate(el) {//动画
+        var i = 0
+            , top = parseInt(el.style.top)
+            , id = setInterval(frame, 24);
+        function frame() {//帧
+            if (i > 180) {
+                clearInterval(id);
+                el.parentNode.removeChild(el);
+            } else {
+                i += 2;
+                el.style.top = top - i + 'px';
+                el.style.opacity = (180 - i) / 180;
+            }
+        }
+    }
+}());
